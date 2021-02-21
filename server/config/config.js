ae12716f725818f4e8420c02deaae810a7c42b85
DZAKYALR <0dzakyalr@gmail.com>
2021-02-21 17:46:22 +0700

    deploy

--- server/config/config.js
+++ server/config/config.js
@@ -25,11 +25,12 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres",
   },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+  "production": {
+    "use_env_variable": "DATABASE_URL",
+    "dialectOptions":{
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
+  }
 };
